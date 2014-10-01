dd7a8da27c94276844c28b36da758e0090af7375
gil-cano <gil.cano@gmail.com>
2014-10-01 08:55:19 -0500

    some basic route examples

--- routes.py
+++ routes.py
@@ -1,11 +1,36 @@
 from flask import Flask
+from flask import render_template
+from flask.ext.bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def home():
-    return '<h1>Hello Flask!</h1>'
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
