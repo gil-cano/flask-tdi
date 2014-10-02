7ce73abc9c7e91072e4310a8bbf7413ece0bbe13
Gildardo Bautista <gil.cano@gmail.com>
2014-10-02 15:02:51 -0500

    add form

--- routes.py
+++ routes.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask import render_template
 from flask.ext.bootstrap import Bootstrap
-
+from flask.ext.wtf import Form
+from wtforms import StringField
+from wtforms import SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 
 @app.route('/')
@@ -30,6 +34,21 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+@app.route('/tickets', methods=['GET', 'POST'])
+def tickets():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('tickets.html', form=form, name=name)
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 
 if __name__ == '__main__':
