0c4fee9cad741b8a0014f8183123e96f052d52c5
Gildardo Bautista <gil.cano@gmail.com>
2014-10-06 14:59:21 -0500

    Add sql example

--- routes.py
+++ routes.py
@@ -6,14 +6,23 @@ from flask import render_template
 from flask import session
 from flask import url_for
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.wtf import Form
 from wtforms import StringField
 from wtforms import SubmitField
 from wtforms.validators import Required
 
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -41,25 +50,65 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
+# @app.route('/tickets', methods=['GET', 'POST'])
+# def tickets():
+#     form = NameForm()
+#     if form.validate_on_submit():
+#         old_name = session.get('name')
+#         if old_name is not None and old_name != form.name.data:
+#             flash('Looks like you have change your name!')
+#         session['name'] = form.name.data
+#         form.name.data = ''
+#         return redirect(url_for('tickets'))
+#     return render_template(
+#         'tickets.html',
+#         form=form,
+#         name=session.get('name'))
+
 @app.route('/tickets', methods=['GET', 'POST'])
 def tickets():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have change your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('tickets'))
     return render_template(
         'tickets.html',
         form=form,
-        name=session.get('name'))
+        name=session.get('name'),
+        known=session.get('known', False))
 
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 if __name__ == '__main__':
